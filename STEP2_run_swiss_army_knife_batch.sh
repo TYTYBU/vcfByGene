bfc74f4b06c3712b9f8390bdaf5e8af34db83d9b
TYTYBU <42722863+TYTYBU@users.noreply.github.com>
2022-10-03 00:56:33 -0400

    added loop for processing blocks

--- STEP2_run_swiss_army_knife_batch.sh
+++ STEP2_run_swiss_army_knife_batch.sh
@@ -7,20 +7,41 @@ VCF_PATH="Bulk/Exome\ sequences/Population\ level\ exome\ OQFE\ variants,\ pVCF\
 dx mkdir -p $BED_PATH
 dx mkdir -p $VCF_OUT_PATH
 
-for f in $BED_PATH_LOCAL/c1_b0_*.bed
+for i in {0..20} # process block 0-20
 do
-	f2=$(basename $f .bed)
-	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
+	for f in $BED_PATH_LOCAL/c1_b${i}_*.bed # edit here to process different chromosome and blocks
+  do
+  	f2=$(basename $f .bed)
+  	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
 
-	IFS='_' read -ra ADDR <<< "$f2"
-	BLK_STR="${ADDR[0]}_${ADDR[1]}"
-	GENE_STR=${ADDR[2]}
+  	IFS='_' read -ra ADDR <<< "$f2"
+  	BLK_STR="${ADDR[0]}_${ADDR[1]}"
+  	GENE_STR=${ADDR[2]}
 
-	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
-	BED_FILE='bed/c1_b1_ACAP3.bed'
+  	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
+  	BED_FILE=$BED_PATH/${f2}.bed
 
-	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
-	echo $CMD_STRING
+  	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
+  	echo $CMD_STRING
 
-	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+  	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+  done
 done
+
+# for f in $BED_PATH_LOCAL/c1_b1_*.bed
+# do
+# 	f2=$(basename $f .bed)
+# 	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
+#
+# 	IFS='_' read -ra ADDR <<< "$f2"
+# 	BLK_STR="${ADDR[0]}_${ADDR[1]}"
+# 	GENE_STR=${ADDR[2]}
+#
+# 	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
+# 	BED_FILE=$BED_PATH/${f2}.bed
+#
+# 	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
+# 	echo $CMD_STRING
+#
+# 	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+# done
