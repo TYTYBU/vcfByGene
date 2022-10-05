f6c8e5a02e4bf6371c3fc41bd7ff624ca22e46ea
TYTYBU <42722863+TYTYBU@users.noreply.github.com>
2022-10-05 16:03:07 -0400

    added functionality to provide a gene list to process the VCFs

--- STEP1_prepare_bed_files.sh
+++ STEP1_prepare_bed_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
 
-BED_PATH_LOCAL='mane_gene_bed'
+BED_PATH_LOCAL='temp'
 RESOURCE_PATH='resources'
 mkdir -p $BED_PATH_LOCAL
-
 perl mane2bed.pl $RESOURCE_PATH/mane_track_UCSC_v1.0.txt $RESOURCE_PATH/pvcf_blocks.txt $BED_PATH_LOCAL
+tar czf mane_bed.tar.gz $BED_PATH_LOCAL/*.bed
+rm -rf $BED_PATH_LOCAL
--- STEP2_run_swiss_army_knife_batch.sh
+++ STEP2_run_swiss_army_knife_batch.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -l
-
-BED_PATH='bed'
-BED_PATH_LOCAL='mane_gene_bed'
-VCF_OUT_PATH='vcf_out'
+BED_PATH='bed_50genes'
+BED_PATH_LOCAL='temp' # need to be the same as the mane_bed.tar.gz extraction folder
+VCF_OUT_PATH='vcf_out_50genes'
 VCF_PATH="Bulk/Exome\ sequences/Population\ level\ exome\ OQFE\ variants,\ pVCF\ format\ -\ final\ release"
 dx mkdir -p $BED_PATH
 dx mkdir -p $VCF_OUT_PATH
 
-for i in {0..20} # process block 0-20
-do
-	for f in $BED_PATH_LOCAL/c1_b${i}_*.bed # edit here to process different chromosome and blocks
+tar -zxf mane_bed.tar.gz # extract bed files to ./temp/
+
+# process VCFs by a list of genes
+RESOURCE_PATH='resources'
+GENE_LIST=$RESOURCE_PATH/gene_list_50.txt
+while read gene; do
+	for f in $BED_PATH_LOCAL/*_${gene}.bed # edit here to process different chromosome and blocks
   do
+		# echo $f
   	f2=$(basename $f .bed)
   	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
 
@@ -26,22 +30,30 @@ do
 
   	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
   done
-done
+done <${GENE_LIST}
 
-# for f in $BED_PATH_LOCAL/c1_b1_*.bed
+
+
+# # process VCFs by blocks
+# for i in {0..20} # process block 0-20
 # do
-# 	f2=$(basename $f .bed)
-# 	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
+# 	for f in $BED_PATH_LOCAL/c1_b${i}_*.bed # edit here to process different chromosome and blocks
+#   do
+#   	f2=$(basename $f .bed)
+#   	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
 #
-# 	IFS='_' read -ra ADDR <<< "$f2"
-# 	BLK_STR="${ADDR[0]}_${ADDR[1]}"
-# 	GENE_STR=${ADDR[2]}
+#   	IFS='_' read -ra ADDR <<< "$f2"
+#   	BLK_STR="${ADDR[0]}_${ADDR[1]}"
+#   	GENE_STR=${ADDR[2]}
 #
-# 	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
-# 	BED_FILE=$BED_PATH/${f2}.bed
+#   	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
+#   	BED_FILE=$BED_PATH/${f2}.bed
 #
-# 	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
-# 	echo $CMD_STRING
+#   	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
+#   	echo $CMD_STRING
 #
-# 	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+#   	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+#   done
 # done
+
+rm -rf $BED_PATH_LOCAL
