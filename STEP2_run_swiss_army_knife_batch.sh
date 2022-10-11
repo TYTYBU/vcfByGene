84edf1da6bc0a668a73eeea12740e575ade2918a
TYTYBU <42722863+TYTYBU@users.noreply.github.com>
2022-10-11 17:03:26 -0400

    added capability to process genes not on MANE track

--- STEP2_run_swiss_army_knife_batch.sh
+++ STEP2_run_swiss_army_knife_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-BED_PATH='bed_50genes'
+BED_PATH='selected_genes_bed'
 BED_PATH_LOCAL='temp' # need to be the same as the mane_bed.tar.gz extraction folder
-VCF_OUT_PATH='vcf_out_50genes'
+VCF_OUT_PATH='vcf_out_selected_genes'
 VCF_PATH="Bulk/Exome\ sequences/Population\ level\ exome\ OQFE\ variants,\ pVCF\ format\ -\ final\ release"
 dx mkdir -p $BED_PATH
 dx mkdir -p $VCF_OUT_PATH
@@ -10,25 +10,56 @@ tar -zxf mane_bed.tar.gz # extract bed files to ./temp/
 
 # process VCFs by a list of genes
 RESOURCE_PATH='resources'
-GENE_LIST=$RESOURCE_PATH/gene_list_50.txt
+
+GENE_LIST=$RESOURCE_PATH/genes_not_on_MANE.txt
+while read gene; do
+	for f in $BED_PATH_LOCAL/*_${gene}_*.bed # edit here to process different chromosome and blocks
+  do
+		if [ -f "$f" ]; then
+			# echo $f
+	  	f2=$(basename $f .bed)
+	  	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
+
+	  	IFS='_' read -ra ADDR <<< "$f2"
+	  	BLK_STR="${ADDR[0]}_${ADDR[1]}"
+	  	GENE_STR=${ADDR[2]}_${ADDR[3]}_${ADDR[4]} # refseq_id after gene symbol
+
+	  	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
+	  	BED_FILE=$BED_PATH/${f2}.bed
+
+	  	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
+	  	echo $CMD_STRING
+
+	  	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination $VCF_OUT_PATH -y
+		else
+			echo "$f does not exist."
+		fi
+  done
+done <${GENE_LIST}
+
+GENE_LIST=$RESOURCE_PATH/genes_on_MANE.txt
 while read gene; do
 	for f in $BED_PATH_LOCAL/*_${gene}.bed # edit here to process different chromosome and blocks
   do
-		# echo $f
-  	f2=$(basename $f .bed)
-  	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
+		if [ -f "$f" ]; then
+			# echo $f
+	  	f2=$(basename $f .bed)
+	  	dx upload --brief --wait --path $BED_PATH/$f2.bed $f
 
-  	IFS='_' read -ra ADDR <<< "$f2"
-  	BLK_STR="${ADDR[0]}_${ADDR[1]}"
-  	GENE_STR=${ADDR[2]}
+	  	IFS='_' read -ra ADDR <<< "$f2"
+	  	BLK_STR="${ADDR[0]}_${ADDR[1]}"
+	  	GENE_STR=${ADDR[2]}
 
-  	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
-  	BED_FILE=$BED_PATH/${f2}.bed
+	  	VCF_PREFIX="ukb23157_${BLK_STR}_v1"
+	  	BED_FILE=$BED_PATH/${f2}.bed
 
-  	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
-  	echo $CMD_STRING
+	  	CMD_STRING="bcftools view --threads 4 -O z -R "$(basename $BED_FILE)" ${VCF_PREFIX}.vcf.gz > ${GENE_STR}.vcf.gz"
+	  	echo $CMD_STRING
 
-  	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination vcf_out -y
+	  	dx run swiss-army-knife -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz" -iin="$VCF_PATH/${VCF_PREFIX}.vcf.gz.tbi" -iin="$BED_FILE" -icmd="$CMD_STRING" --destination $VCF_OUT_PATH -y
+		else
+			echo "$f does not exist."
+		fi
   done
 done <${GENE_LIST}
 
@@ -57,3 +88,4 @@ done <${GENE_LIST}
 # done
 
 rm -rf $BED_PATH_LOCAL
+rm -rf mane_bed.tar.gz
---
+++ refseq2bed.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+$infile = $ARGV[0]; # refseq all fields txt file
+$infile2 = $ARGV[1]; # pvcf_blocks txt file
+$out_path = $ARGV[2]; # output BED file path
+
+if ($out_path eq ""){
+  $out_path = ".";
+}
+
+open IN, "<", $infile or die;
+open IN2, "<", $infile2 or die;
+
+while (my $line = <IN2>){
+  chomp $line;
+  my ($ind, $chr, $blk, $start_pos, $end_pos) = split /\t/, $line;
+  if ($chr == 23){
+    $chr = "cX";
+  } elsif ($chr == 24){
+    $chr = "cY";
+  } else {
+    $chr = "c" . $chr;
+  }
+  $blk = "b" . $blk;
+
+  $hash{$chr}{$blk}{start} = $start_pos;
+  $hash{$chr}{$blk}{end} = $end_pos;
+}
+
+<IN>;
+while (my $line = <IN>){
+  if($line =~ /[^\t]+\t([^\t]+)\t([^\t]+)\t[^\t]+\t([^\t]+)\t([^\t]+)\t[^\t]+\t[^\t]+\t[^\t]+\t([^\t]+)\t([^\t]+)\t[^\t]+\t([^\t]+)/){
+    my $gene_id = $1;
+    my $chr = $2;
+    my $txStart = $3;
+    my $txEnd = $4;
+    my $exonStart_str = $5;
+    my $exonEnd_str = $6;
+    my $gene_symbol = $7;
+
+    my $chr2 = substr $chr, 3;
+    $chr2 = "c" . $chr2;
+
+    foreach my $blk (keys %{$hash{$chr2}}){
+      if ((($hash{$chr2}{$blk}{start} < $txEnd) and ($hash{$chr2}{$blk}{end} > $txEnd)) or (($hash{$chr2}{$blk}{start} < $txStart) and ($hash{$chr2}{$blk}{end} > $txStart))){
+        $exonStart_str = substr $exonStart_str, 0, -1;
+        @exonStart = split /,/, $exonStart_str;
+        $exonEnd_str = substr $exonEnd_str, 0, -1;
+        @exonEnd = split /,/, $exonEnd_str;
+
+        open OUT, ">", $out_path . "/" . $chr2 . "_" . $blk . "_" . $gene_symbol . "_" . $gene_id . ".bed";
+        foreach my $i (0 .. (scalar(@exonStart)-1)){
+          print OUT $chr . "\t" . $exonStart[$i] . "\t" . $exonEnd[$i] . "\t" . $gene_symbol . "\n";
+        }
+        close OUT;
+
+        last;
+      }
+    }
+  }
+}
